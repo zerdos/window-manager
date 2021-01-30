2306d2b345210c5e6fad6fd30cccf1eac4e2ae8e
Zoltan Erdos <zoltan.erdos@me.com>
2021-01-30 12:07:20 +0000

    esbuild

--- public/simple-window-manager.es.js
+++ public/simple-window-manager.es.js
@@ -30,18 +30,9 @@ function html(options={})
     return object
 }
 
-function createCommonjsModule(fn, basedir, module) {
-	return module = {
-	  path: basedir,
-	  exports: {},
-	  require: function (path, base) {
-      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-    }
-	}, fn(module, module.exports), module.exports;
-}
-
-function commonjsRequire () {
-	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+function createCommonjsModule(fn) {
+  var module = { exports: {} };
+	return fn(module, module.exports), module.exports;
 }
 
 var eventemitter3 = createCommonjsModule(function (module) {
